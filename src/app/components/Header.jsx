13a6da6e32f1a6324b39cf021b9a5f293f1b0244
inedumozey <inedumozey@email.com>
2024-01-06 00:26:38 +0100

    updated

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,12 +5,13 @@ const sevillana = Sevillana({ subsets: ['latin'], weight: ['400'] })
 
 export default function Header() {
     return (
-        <div className='text-black p-[10px] relative' >
-            {/* <img src='/bg1.jpg' className='h-[100%]' /> */}
+        <div className='text-white p-[10px]' >
+            <div className={`${acme.className} font-bold text-[2rem]`}><span className={sevillana.className}>A</span>rogundade</div>
+            <div className='text-center'>
+                <div>Mrs Arogundade of blessed memory</div>
+                <div className='text-[.8rem]'>12<sup>th</sup> Oct, 1975 - 21<sup>st</sup> Nov, 2023</div>
+            </div>
         </div>
     )
 }
 
-{/* <div className={`${acme.className} font-bold text-[2rem]`}><span className={sevillana.className}>A</span>rogundade</div>
-            <div>Mrs Arogundade of blessed memory</div>
-            <div>12<sup>th</sup> Oct; 1975 - 21<sup>st</sup> Nov; 2023</div> */}
